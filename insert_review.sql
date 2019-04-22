7b4cc48e216c6695e68755d88158b6b3dce6e3f0
adroit22 <mathur.samiksha7@gmail.com>
2019-04-22 01:42:30 -0400

    minor bug fixes

--- insert_review.sql
+++ insert_review.sql
@@ -3,12 +3,12 @@ VALUES
 (1001, 104, 5, 6, 6, 7, 'needs improvement', 'Bronn@gmail.com'),
 (1002, 101, 6, 5, 6, 8, 'well written', 'bstark@got.com'),
 (1003, 103, 6, 7, 7, 8, 'great effort', 'bstark@got.com'),
-(1004, 101, 7, 5, 5, 7, 'nicely explained', 'cersei@reg.com'),
+(1004, 101, 7, 5, 5, 7, 'nicely explained', 'cersei@got.com'),
 (1005, 102, 5, 7, 4, 6, 'more research', 'Bronn@gmail.com'),
 (1006, 101, 7, 6, 6, 7, 'nice', 'sansa@yahoo.com'),
 (1007, 102, 5, 5, 7, 7, 'more effort required', 'Gendry@gto.com'),
-(1008, 102, 8, 4, 6, 7, 'average', 'melisandre@nyu.com'),
-(1009, 103, 7, 7, 7, 7, 'great effort', 'sandor@amu.com'),
+(1008, 102, 8, 4, 6, 7, 'average', 'melisandre@nyu.edu'),
+(1009, 103, 7, 7, 7, 7, 'great effort', 'sandor@amu.edu'),
 (1010, 103, 8, 7, 8, 7, 'well formed', 'lanni@rit.com'),
-(1003, 104, 6, 5, 7, 6, 'needs improvement', 'cersei@reg.com');
+(1011, 104, 6, 5, 7, 6, 'needs improvement', 'cersei@got.com');
 
